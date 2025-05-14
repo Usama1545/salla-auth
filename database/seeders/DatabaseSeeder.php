0b702f172cc8d8f24d649c1aa3a76c8a46bb22de
Usama yasin <yasinusama414@gmail.com>
2025-05-14 19:58:35 +0500

    seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
+        User::create([
+            'name' => 'test',
             'email' => 'test@example.com',
+            'password' => bcrypt('123456'),
         ]);
     }
 }
